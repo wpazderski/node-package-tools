c7880f97e169910c1b96cfb7a2642adebc49a8df
Witold Pazderski <witold.pazderski@gmail.com>
2026-06-16 10:48:44 +0200

    Update dependencies

--- package.config.ts
+++ package.config.ts
@@ -19,5 +19,5 @@ const config: NodePackageToolsConfig = mergeConfigs(configs.base.latest, {
     },
 });
 
-// eslint-disable-next-line import/no-default-export
+// eslint-disable-next-line import-x/no-default-export
 export default config;
